1bf0d02f33efd2c3f232e1d0e518dd4a15bf274c
Melvin Mupondori <melmups@outlook.com>
2019-04-14 02:02:25 +0200

    Finished up 'parser'

--- program.py
+++ program.py
@@ -23,3 +23,25 @@ if(not storageLocation or not isdir(storageLocation)):
 # Shit is about to go down
 print(f"\r\nFetching {str(total)} pages from '{base}'", end="\r\n\r\n")
 
+# Loop and parse each page
+for i in range(1, total+1):
+    print(f"Parsing page {str(i)} out of {str(total)}")
+
+    # Get the html of the page as an html string
+    html = read_url_as_string(f"{base}?page={str(i)}")
+
+    # Get the download links of the PDFs
+    # Yes, yes. Using regex on HTML strings in never good due to walla walla. But 
+    # in this use case it doesn't really matter much. We control the output so meh
+    links = regex_search(r'<a href="(.*)">Download[\s\S]PDF<\/a>', html)
+    
+    # Push links to collection
+    final.extend(links)
+
+
+# Check how many links we got
+print(f"\r\n\r\nParsing complete. Starting document downloads. {str(len(final))} downloads in queue")
+
+for i, link in enumerate(final):
+    print(f"Downloading file {str(i+1)} of {str(len(final))}")
+    download_file(link, storageLocation)
